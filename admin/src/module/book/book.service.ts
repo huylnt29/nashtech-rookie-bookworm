45badf2ae01c67720cd27e12e4ce790b50ccfa28
Huy Le Nguyen Truong <lnthuy29012002@gmail.com>
2024-05-17 15:43:22 +0700

    Feature/category/view detail/api (#7)
    * Wrote GET API by Category id
    
    * Select some fields from book, not all
    
    * Added some Book API to test Category easier

--- admin/src/module/book/book.service.ts
+++ admin/src/module/book/book.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/core/prisma/prisma.service';
 import { Book } from '@prisma/client';
+import { CreateBookDto } from './dto/create_book.dto';
 
 @Injectable()
 export class BookService {
   constructor(private prisma: PrismaService) {}
 
-  async books(): Promise<Book[]> {
-     return this.prisma.book.findMany();
-   }
+  async selectBooks(): Promise<Book[]> {
+    return this.prisma.book.findMany();
+  }
+
+  async insertBook(createBookDto: CreateBookDto) {
+    return this.prisma.book.create({
+      data: {
+        name: createBookDto.name,
+        description: createBookDto.description,
+        publishedYear: createBookDto.publishedYear,
+        publisherId: createBookDto.publisherId,
+        categoryId: createBookDto.categoryId,
+        authors: {
+          connect: createBookDto.authorIds.map((e) => ({
+            id: e,
+          })),
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+  }
 }
