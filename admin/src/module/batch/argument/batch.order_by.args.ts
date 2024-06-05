488f75ad518d2da7004bd7b836676ba53922b121
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-06-05 09:31:29 +0700

    Updated Batch order by

--- admin/src/module/batch/argument/batch.order_by.args.ts
+++ admin/src/module/batch/argument/batch.order_by.args.ts
@@ -1,10 +1,16 @@
 import { Field, InputType, PartialType } from '@nestjs/graphql';
 import { Prisma } from '@prisma/client';
+import { BookOrderByWithRelationInput } from 'src/module/book/argument/book.order_by.args';
+import { DiscountOrderByWithRelationInput } from 'src/module/discount/argument/discount.order_by.args';
 
 @InputType()
 export class BatchOrderByWithRelationInputStrict {
   @Field(() => Prisma.SortOrder)
   price: Prisma.SortOrder;
+  @Field(() => DiscountOrderByWithRelationInput)
+  discount: DiscountOrderByWithRelationInput;
+  @Field(() => BookOrderByWithRelationInput)
+  book: BookOrderByWithRelationInput;
 }
 
 @InputType()
