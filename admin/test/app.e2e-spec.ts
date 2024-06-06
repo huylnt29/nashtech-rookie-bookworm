18a42e41dead67c79034fc872d8649560185dacc
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-06-06 21:58:59 +0700

    Implemented unit test and E2E test

--- admin/test/app.e2e-spec.ts
+++ admin/test/app.e2e-spec.ts
@@ -1,24 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
 
-describe('AppController (e2e)', () => {
+import { INestApplication } from '@nestjs/common';
+
+import { PrismaService } from 'src/core/service/prisma/prisma.service';
+import { S3Service } from 'src/core/service/s3/s3.service';
+import { AuthorService } from 'src/module/author/author.service';
+import { BookController } from 'src/module/book/book.controller';
+import { BookService } from 'src/module/book/book.service';
+import { CategoryService } from 'src/module/category/category.service';
+import { PublisherService } from 'src/module/publisher/publisher.service';
+
+describe('BookController (e2e)', () => {
   let app: INestApplication;
+  let bookService: BookService;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
+    const moduleRef = await Test.createTestingModule({
+      controllers: [BookController],
+      providers: [
+        PrismaService,
+        BookService,
+        S3Service,
+        AuthorService,
+        PublisherService,
+        CategoryService,
+      ],
+    })
+      .overrideProvider(BookService)
+      .useValue(bookService)
+      .compile();
+    bookService = moduleRef.get<BookService>(BookService);
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it(`GET /book/everything`, async () => {
+    const res = await request(app.getHttpServer()).get('/book/everything');
+    expect(res.status).toBe(200);
+    expect(res.body).toMatchObject(expect.arrayContaining([]));
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
