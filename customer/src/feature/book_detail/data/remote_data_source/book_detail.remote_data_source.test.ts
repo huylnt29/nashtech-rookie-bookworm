251f99d1ba2953f28e5d14dfe98ae7bd36dc22a7
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-06-07 13:54:19 +0700

    Removed redundant codes

--- customer/src/feature/book_detail/data/remote_data_source/book_detail.remote_data_source.test.ts
+++ customer/src/feature/book_detail/data/remote_data_source/book_detail.remote_data_source.test.ts
@@ -1,47 +1,8 @@
-import { describe, it, expect, vi } from "vitest";
-import { BookDetail } from "../model/book_detail.type";
+import { describe, it, expect } from "vitest";
 import ApiClient from "../../../../core/network/remote/api_client";
 
 describe("BookDetailRemoteDataSource", () => {
   it("Should return a book", async () => {
-    // just a dummy quote
-    const dummyBook: BookDetail = {
-      id: 1,
-      name: "",
-      batchId: 0,
-      imageUrls: [],
-      description: "",
-      publishedYear: 0,
-      totalSoldQuantity: 0,
-      averageRating: 0,
-      remainingQuantity: 0,
-      price: 0,
-      publisher: {
-        id: 0,
-        name: "",
-      },
-      category: {
-        id: 0,
-        name: "",
-      },
-      authors: [],
-      discount: {
-        id: 0,
-        percentage: 0,
-        minQuantity: 0,
-        maxQuantity: "",
-        endAt: "",
-      },
-      reviews: [],
-    };
-
-    const mockResponse = {
-      ok: true,
-      statusText: "OK",
-      json: async () => dummyBook,
-    } as Response;
-
-    globalThis.fetch = vi.fn().mockResolvedValue(mockResponse);
     expect(await ApiClient.getBookDetail("1")).toHaveProperty("id", 1);
   });
 });
